a68a60962fd59cbb792c2cec31f43fe6b2675dc2
Jorge Esqueda Rocha <jorge_squiky@hotmail.com>
2026-07-24 12:26:35 -0600

    React chart, added a customizable component

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+    {
+        name: 'Jan',
+        "Active User": 400,
+    },
+    {
+        name: 'Feb',
+        "Active User": 300,
+    },
+    {
+        name: 'Mar',
+        "Active User": 300,
+    },
+    {
+        name: 'Apr',
+        "Active User": 200,
+    },
+    {
+        name: 'May',
+        "Active User": 278,
+    },
+    {
+        name: 'Jun',
+        "Active User": 189,
+    },
+    {
+        name: 'Jul',
+        "Active User": 289,
+    },
+    {
+        name: 'Aug',
+        "Active User": 139,
+    },
+    {
+        name: 'Sep',
+        "Active User": 180,
+    },
+    {
+        name: 'Oct',
+        "Active User": 200,
+    },
+    {
+        name: 'Nov',
+        "Active User": 180,
+    },
+    {
+        name: 'Dic',
+        "Active User": 100,
+    },
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
+import Chart from "../../components/chart/Chart"
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo"
+import { userData } from "../../dummyData";
+
 import "./home.scss"
 
 const Home = () => {
     return (
         <div className="home">
             <FeaturedInfo />
+            <Chart
+              data={ userData }
+              title="User Analytics"
+              grid
+              dataKey={"Active User"}
+            />
         </div>
     )
 }
