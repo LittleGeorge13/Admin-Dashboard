32feacbfaa6d97413bedf13efabb5bbf44b4fe12
Jorge Esqueda Rocha <jorge_squiky@hotmail.com>
2026-07-24 13:31:52 -0600

    Created user information component

---
+++ src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,89 @@
+import { Visibility } from "@material-ui/icons"
+import "./widgetSm.scss"
+
+const WidgetSm = () => {
+    return (
+        <div className="widgetSm">
+            <span className="widgetSmTitle">New Join Members</span>
+            <ul className="widgetSmList">
+                <li className="widgetSmListItem">
+                    <img
+                      src="https://static.vecteezy.com/system/resources/thumbnails/024/677/993/small/3d-icon-of-men-profil-people-free-png.png"
+                      alt=""
+                      className="widgetSmImg"
+                    />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon" />
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img
+                      src="https://static.vecteezy.com/system/resources/thumbnails/024/677/993/small/3d-icon-of-men-profil-people-free-png.png"
+                      alt=""
+                      className="widgetSmImg"
+                    />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon" />
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img
+                      src="https://static.vecteezy.com/system/resources/thumbnails/024/677/993/small/3d-icon-of-men-profil-people-free-png.png"
+                      alt=""
+                      className="widgetSmImg"
+                    />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon" />
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img
+                      src="https://static.vecteezy.com/system/resources/thumbnails/024/677/993/small/3d-icon-of-men-profil-people-free-png.png"
+                      alt=""
+                      className="widgetSmImg"
+                    />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon" />
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img
+                      src="https://static.vecteezy.com/system/resources/thumbnails/024/677/993/small/3d-icon-of-men-profil-people-free-png.png"
+                      alt=""
+                      className="widgetSmImg"
+                    />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon" />
+                        Display
+                    </button>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default WidgetSm
