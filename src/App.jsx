71c3519fe502d5689fbdf323103480aa722cf4ba
Jorge Esqueda Rocha <jorge_squiky@hotmail.com>
2026-07-27 10:47:10 -0600

    Created edit user page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./app.scss"
 import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 
 const App = () => {
   return(
@@ -15,6 +16,7 @@ const App = () => {
         <Routes>
           <Route path="/" Component={ Home }/>
           <Route path="/users" Component={ UserList }/>
+          <Route path="/user/:userId" Component={ User }/>
         </Routes>
       </div>
     </Router>
---
+++ src/pages/user/User.jsx
@@ -0,0 +1,115 @@
+import { CalendarToday, LocationSearching, MailOutline, PermIdentity, PhoneAndroid, Publish } from "@material-ui/icons"
+import "./user.scss"
+
+const User = () => {
+    return (
+        <div className='user'>
+            <div className="userTitleContainer">
+                <h1 className="userTitle">Edit user</h1>
+                <button className="userAddButton">Create</button>
+            </div>
+            <div className="userContainer">
+                <div className="userShow">
+                    <div className="userShowTop">
+                        <img
+                          src="https://static.vecteezy.com/system/resources/thumbnails/024/677/993/small/3d-icon-of-men-profil-people-free-png.png"
+                          alt=""
+                          className="userShowImg"
+                        />
+                        <div className="userShowTopTitle">
+                            <span className="userShowUsername">Anna Becker</span>
+                            <span className="userShowUserTitle">Software Engineer</span>
+                        </div>
+                    </div>
+                    <div className="userShowBottom">
+                        <span className="userShowTitle">Account Details</span>
+                        <div className="userShowInfo">
+                            <PermIdentity className="userShowIcon"/>
+                            <span className="userShowInfoTitle">squiky13</span>
+                        </div>
+                        <div className="userShowInfo">
+                            <CalendarToday className="userShowIcon"/>
+                            <span className="userShowInfoTitle">23.11.1998</span>
+                        </div>
+                        <span className="userShowTitle">Contact Details</span>
+                        <div className="userShowInfo">
+                            <PhoneAndroid className="userShowIcon"/>
+                            <span className="userShowInfoTitle">+52 449 123 1234</span>
+                        </div>
+                        <div className="userShowInfo">
+                            <MailOutline className="userShowIcon"/>
+                            <span className="userShowInfoTitle">test@email.com</span>
+                        </div>
+                        <div className="userShowInfo">
+                            <LocationSearching className="userShowIcon"/>
+                            <span className="userShowInfoTitle">New York | USA</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="userUpdate">
+                    <span className="userUpdateTitle">Edit</span>
+                    <form className="userUpdateForm">
+                        <div className="userUpdateLeft">
+                            <div className="userUpdateItem">
+                                <label>Username</label>
+                                <input
+                                  type="text"
+                                  placeholder="username"
+                                  className="userUpdateInput"
+                                />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Fullname</label>
+                                <input
+                                  type="text"
+                                  placeholder="Full name"
+                                  className="userUpdateInput"
+                                />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Email</label>
+                                <input
+                                  type="text"
+                                  placeholder="example@email.com"
+                                  className="userUpdateInput"
+                                />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Phone</label>
+                                <input
+                                  type="text"
+                                  placeholder="+524441231234"
+                                  className="userUpdateInput"
+                                />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Address</label>
+                                <input
+                                  type="text"
+                                  placeholder="New York | USA"
+                                  className="userUpdateInput"
+                                />
+                            </div>
+                        </div>
+                        <div className="userUpdateRight">
+                            <div className="userUpdateUpload">
+                                <img
+                                  src="https://static.vecteezy.com/system/resources/thumbnails/024/677/993/small/3d-icon-of-men-profil-people-free-png.png"
+                                  alt=""
+                                  className="userUpdateImg"
+                                />
+                                <label htmlFor="file">
+                                    <Publish className="userUpdateIcon" />
+                                </label>
+                                <input type="file" id='file' style={{ display: "none" }}/>
+                            </div>
+                            <button className="userUpdateButton">Update</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default User
